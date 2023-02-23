15373e278e996d8a1ae7334136ca141d9d653c63
sunjoolee <spottypoo124@gmail.com>
2023-02-23 20:47:04 +0900

    CH8 테스트 작성
    에지 케이스를 위한 케이스

--- app/src/main/java/learningconcurrencyinkotlin/debuggingexample/chapter8/SampleAppFT.kt
+++ app/src/main/java/learningconcurrencyinkotlin/debuggingexample/chapter8/SampleAppFT.kt
@@ -20,7 +20,12 @@ class SampleAppFT {
     }
     @Test
     fun testOppositeOrder() = runBlocking {
-        //TODO: 예상이 어려운 검색 순서에 대한 테스트 구현
+        val manager = UserManager(MockSlowDataSource())
+        val user = manager.getUser(10)
+
+        assertTrue(user.name == "Susan Calvin")
+        assertTrue(user.age ==  Calendar.getInstance().get(Calendar.YEAR) - 1982)
+        assertTrue(user.profession == "Robopsychologist")
     }
 }
 
@@ -38,6 +43,24 @@ class MockDataSource:DataSource{
     //외부시스템에서 profession을 가져오는 모의 기능
     override fun getProfessionAsync(id: Int) = GlobalScope.async{
         delay(2000)
+        "Robopsychologist"
+    }
+}
+
+class MockSlowDataSource:DataSource{
+    //데이터베이스에서 name을 가져오는 모의 기능
+    override fun getNameAsync(id: Int) = GlobalScope.async{
+        delay(1000)
+        "Susan Calvin"
+    }
+    //캐시에서 age를 가져오는 모의 기능
+    override fun getAgeAsync(id: Int) = GlobalScope.async{
+        delay(500)
+        Calendar.getInstance().get(Calendar.YEAR) - 1982
+    }
+    //외부시스템에서 profession을 가져오는 모의 기능
+    override fun getProfessionAsync(id: Int) = GlobalScope.async{
+        delay(200)
         "Robopsychologist"
     }
 }
