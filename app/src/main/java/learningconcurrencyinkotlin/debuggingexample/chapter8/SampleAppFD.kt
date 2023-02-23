651d68f407e69335e661290f4dbfb972f4385fc6
sunjoolee <spottypoo124@gmail.com>
2023-02-23 21:28:13 +0900

    CH8 디버깅
    로그에서 코루틴 식별

---
+++ app/src/main/java/learningconcurrencyinkotlin/debuggingexample/chapter8/SampleAppFD.kt
@@ -0,0 +1,31 @@
+package learningconcurrencyinkotlin.debuggingexample.chapter8
+
+import kotlinx.coroutines.*
+import org.junit.Test
+
+class SampleAppFD {
+    @Test
+    fun testSampleAppFD() = runBlocking{
+        val pool = newFixedThreadPoolContext(3, "myPool")
+        val ctx = newSingleThreadContext("ctx")
+
+        val tasks = mutableListOf<Deferred<Unit>>()
+        for (i in 0..5) {
+            val task = GlobalScope.async(pool) {
+                println("Processing ${i} in ${threadName()}")
+
+                withContext(ctx) {
+                    println("Step two of ${i} happening in thread ${threadName()}")
+                }
+
+                println("Finishing ${i} in ${threadName()}")
+            }
+            tasks.add(task)
+        }
+
+        for(task in tasks){
+            task.await()
+        }
+    }
+    private fun threadName() = Thread.currentThread().name
+}
